21034ed06a987dd6a74a5a4f29adb29a38420635
Sidectrl <limonellifrancesco@gmail.com>
2023-05-29 01:16:46 +0200

    removed useless code

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../../images/Logo.png';
@@ -8,15 +7,7 @@ function CustomNavbar() {
   return (
     <>
       <Navbar className='d-flex justify-content-center align-items-center' style={{margin:0, padding:0, backgroundColor:'#0e0e0f'}} variant='dark'>
-        {/*<div className='d-flex align-items-center justify-content-center'>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />*/}
-          <Navbar.Brand style={{margin:0, padding:0}} href='#'><img  src={logo} className="logo" alt="logo"/></Navbar.Brand>
-        {/*</div> 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-          </Nav>
-  </Navbar.Collapse>*/}
+          <Navbar.Brand style={{margin:0, padding:0}} href=''><img  src={logo} className="logo" alt="logo"/></Navbar.Brand>
       </Navbar>
     </>
   );
