7cf116dcd5e5c5c4f4e213cca078e5475da7205f
Sidectrl <limonellifrancesco@gmail.com>
2023-06-19 14:34:08 +0200

    feat(Promo): add promo style and visualization

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import './Promo.css'
+import { promos } from '../../Data/PromoData'
 const Promo = () => {
   return (
-    <div>Promo</div>
+    <div className='col-12 text-center'>
+        <h1>Promo</h1>
+        <div>
+            {promos.map(promo =>
+            <h4>{promo.name} {promo.price ? "€"+promo.price?.toFixed(2): ''}</h4> 
+                )}
+        </div>
+    </div>
   )
 }
 
