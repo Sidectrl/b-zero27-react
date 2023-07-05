a5b31ad0fa77cb86d42e08d1d8cd2660a148041a
Francesco <30480692+sidectrl@users.noreply.github.com>
2023-07-05 20:29:22 +0200

    Feature: footer (#8)
    * feature(Footer):add footer style and footer social icons
    
    * feature(app): add footer component
    
    * feature: install react-icons package
    
    * feature(footer): add bzero27 telephone number
    
    * feature(Footer): add h1 "Contatti", padding and margin

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaFacebookF, FaInstagram, FaTiktok, FaPhoneAlt } from "react-icons/fa";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="container-footer text-center mt-4">
+            <h1>Contatti</h1>
+            <div className="container py-3 ">
+                <div className="row">
+                    <div className="col-12">
+                            <a className="social-button mx-4" href="tel:+39 327 550 9899" role="button">
+                               <FaPhoneAlt style={{fontSize: '25px'}}/>
+                            </a>
+                            <a className="social-button mx-4 w-25" href="https://www.facebook.com/profile.php?id=100088658636092" role="button" target="blank">
+                                <FaFacebookF style={{fontSize: '25px'}}/>
+                            </a>
+                            <a className="social-button mx-4 w-25" href="https://www.instagram.com/bzero27/" role="button" target="blank">
+                               <FaInstagram style={{fontSize: '25px'}}/>
+                            </a>
+                            <a className="social-button mx-4 w-25" href="https://www.tiktok.com/@bzero27?_t=8djwsRik9Mo&_r=1" role="button" target="blank">
+                               <FaTiktok style={{fontSize: '25px'}}/>
+                            </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
